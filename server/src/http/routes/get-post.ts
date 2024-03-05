38c2fbcbceedb55ef93402f6d5ede7facd70ea2a
math7b <hiragi.barth@gmail.com>
2024-03-05 18:06:32 -0300

    feat: Add the tables Post and Comment and get-post route
    The motive for this commit is to add the table post and comments to the
    database and a route to access the post or comment.
    
    This commit add the table Post and Comment the Post is a list of posts
    and the Comment cam also be a comment or be a response to anothe comment
    and the route get-post is to get a post by id and all the comments
    related with that post.

---
+++ server/src/http/routes/get-post.ts
@@ -0,0 +1,23 @@
+import z from "zod"
+import { prisma } from "../../lib/prisma"
+import { FastifyInstance } from "fastify"
+
+export async function getPost(app: FastifyInstance) {
+    app.get('/posts/:postId', async (request, reply) => {
+        const getPostParams = z.object({
+            postId: z.string().uuid(),
+        })
+
+        const { postId } = getPostParams.parse(request.params)
+
+        const post = await prisma.post.findUnique({
+            where: {
+                id: postId,
+            },
+            include: {
+                comments: {}
+            }
+        })
+        return reply.status(201).send({ PostId: postId, post: post })
+    })
+}
--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,6 +1,9 @@
 import { fastify } from 'fastify'
+import { getPost } from './routes/get-post'
 
 const app = fastify()
+
+app.register(getPost)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP Server running!')
