dac1befcf98befd3f0c93fa5ce9921001357c2ea
math7b <hiragi.barth@gmail.com>
2024-03-07 11:47:55 -0300

    feat: Add a route to list the Commits
    The motive for this commit is to add a route that allow the server to
    send a list of Commits in order to allow the user to easily see what the
    communit are doing.
    
    This  commit add a route to list the Commits.

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,11 +1,13 @@
 import { fastify } from 'fastify'
 import { getPost } from './routes/get-post'
 import { getPosts } from './routes/get-posts'
+import { getComments } from './routes/get-comments'
 
 const app = fastify()
 
 app.register(getPost)
 app.register(getPosts)
+app.register(getComments)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP Server running!')
