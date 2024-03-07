49e68bc4622e8d1f7649b9323e01c5e84894f270
math7b <hiragi.barth@gmail.com>
2024-03-07 12:09:16 -0300

    feat: Add a route to list a Commit with it replies
    the motive for this commit is to add a route that allow the server to
    get a Commit by id and it replies as well to allow the user to easily
    see what the communit are replaying to that commit.
    
    This commit add a route to list a Commits by id with it replaies.

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -2,12 +2,14 @@ import { fastify } from 'fastify'
 import { getPost } from './routes/get-post'
 import { getPosts } from './routes/get-posts'
 import { getComments } from './routes/get-comments'
+import { getComment } from './routes/get-comment'
 
 const app = fastify()
 
 app.register(getPost)
 app.register(getPosts)
 app.register(getComments)
+app.register(getComment)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP Server running!')
